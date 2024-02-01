f06608bbb6219dc91b4ad5120f2a66fd1ec4cea2
rajayGmr <rajayghimire1999@gmail.com>
2024-02-01 10:09:40 +0700

    creating Json data

--- 21Json/main.go
+++ 21Json/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type course struct {
+	Name     string `json:"coursename"`
+	Price    int 
+	platform string `json:"Website"`
+	Password string `json:"-"`
+	Tags     []string `json:"tags,omitempty"`
+}
 
 func main() {
 	fmt.Println("Welcome to a class of Json")
+	EncodeJson()
+}
+
+func EncodeJson() {
+	MyCourse := []course{
+		{
+			"Java",
+			1200,
+			"Android",
+			"2344",
+			[]string{"Java", "Android", "nil"},
+		},
+		{
+			"C++",
+			800,
+			"Android",
+			"890324",
+			[]string{"C++", "Android", "Web"},
+		},
+		{
+			"Python",
+			1000,
+			"Android",
+			"sdKxY32334",
+			[]string{"Python", "Android", "Web"},
+		},
+	}
+	//package this data as Json data
+	jsonData, err := json.MarshalIndent(MyCourse, "", "\t")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s\n", jsonData)
+	fmt.Println("--------------------------------")
+	fmt.Println(string(jsonData))
 }
